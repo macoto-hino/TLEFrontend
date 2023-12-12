a5e2e0c76568b2ebde9f87f36b1b5437484102c7
Sam Leung <slyh@hk-fun.com>
2023-12-12 03:50:17 +0000

    Improved LDT visual

--- src/components/common/traffic-sign-button.tsx
+++ src/components/common/traffic-sign-button.tsx
@@ -1,66 +1,64 @@
 import styled from 'styled-components';
 
-const Button = styled.div<{border: string, backgroundColor: string}>`
+const Button = styled.div`
   border-radius: 50%;
-  background-color: ${props => props.backgroundColor};
-  border: ${props => props.border};
   width: 30rem;
   height: 30rem;
-`;
-
-const Sign = styled.div<{color: string, padding: string, lineHeight: string}>`
-  padding: ${props => props.padding};
-  line-height: ${props => props.lineHeight};
-  color: ${props => props.color};
-  font-size: 24rem;
-  font-weight: bold;
-  font-family: "Overpass";
 `;
 
 export default function TrafficSignButton(props: {
   allow: boolean,
   variant: "traffic-light" | "sign",
   sign: "←" | "↑" | "→" | "↶" | "↷",
+  style?: React.CSSProperties,
   onClick: React.MouseEventHandler<HTMLDivElement>
 }) {
-  let border = "4rem solid #01619e";
-  if (props.variant == "sign") {
-    border = props.allow ? "4rem solid #01619e" : "4rem solid #DA2121";
-  } else if (props.variant == "traffic-light") {
-    border = "4rem solid #111111";
-  }
-
+  let border = "#01619e";
   let backgroundColor = "#000000";
-  if (props.variant == "sign") {
-    backgroundColor = props.allow ? "#01619e" : "#EEEEEE";
-  } else if (props.variant == "traffic-light") {
-    backgroundColor = "#111111";
-  }
-
   let signColor = "black";
   if (props.variant == "sign") {
+    border = props.allow ? "#01619e" : "#DA2121";
+    backgroundColor = props.allow ? "#01619e" : "#E0E0E0";
     signColor = props.allow ? "white" : "black";
-  } else if (props.variant == "traffic-light") {
+  }
+  if (props.variant == "traffic-light") {
+    border = "#111111";
+    backgroundColor = "#111111";
     signColor = props.allow ? "#476a4c" : "#a83c19";
   }
 
-  let padding = "0rem 0rem 0rem 1rem";
+  let fontSize = 85;
+  let textX = 50;
+  let textY = 80;
   if (props.sign == "↶") {
-    padding = "0rem 0rem 0rem 0rem";
-  } else if (props.sign == "↷") {
-    padding = "0rem 0rem 0rem 2rem";
-  } else if (props.sign == "↑") {
-    padding = "0rem 0rem 0rem 5rem";
+    textX = 45;
+    textY = 75;
+    fontSize = 80;
   }
-
-  let lineHeight = "22rem";
-  if (props.sign == "↑" || props.sign == "↶" || props.sign == "↷") {
-    lineHeight = "20rem";
+  if (props.sign == "↷") {
+    textX = 55;
+    textY = 75;
+    fontSize = 80;
   }
 
   return (
-    <Button border={border} backgroundColor={backgroundColor} onClick={props.onClick}>
-      <Sign color={signColor} padding={padding} lineHeight={lineHeight}>{props.sign}</Sign>
+    <Button onClick={props.onClick} style={props.style}>
+      <svg width="100%" height="100%" viewBox="0 0 100 100">
+        <circle cx="50" cy="50" r="45" stroke={border} strokeWidth="10" fill={backgroundColor} />
+        <text
+          x={textX}
+          y={textY}
+          dominantBaseline="middle"
+          textAnchor="middle"
+          fontSize={fontSize}
+          fontWeight="700"
+          fontFamily="Overpass"
+          fill={signColor}
+        >
+          {props.sign}
+        </text>
+        {!props.allow && props.variant == "sign" && <rect x="5" y="45" width="90" height="10" transform="rotate(45 50 50)" fill={border} />}
+      </svg>
     </Button>
   );
 }
--- src/components/lane-direction-tool/lane.tsx
+++ src/components/lane-direction-tool/lane.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+
 import TrafficSignButton from '../common/traffic-sign-button';
 
 const Container = styled.div`
@@ -22,15 +23,58 @@ const Box = styled.div`
 `;
 
 export default function Lane(props: {data: LaneDirection, onClick: (eventName: "banLeft" | "banRight" | "banStraight" | "banUTurn") => void}) {
+  const opacity = (allow: boolean) => allow ? 1 : 0.25;
   return (
     <>
       {/* <Box style={{flex: "0 0 60rem", justifyContent: "flex-start"}}>{props.data.label}</Box> */}
       <Container>
-        <Box><TrafficSignButton allow={!props.data.banLeft} variant="sign" sign="←" onClick={() => props.onClick("banLeft")}/></Box>
-        <Box><TrafficSignButton allow={!props.data.banStraight} variant="sign" sign="↑" onClick={() => props.onClick("banStraight")}/></Box>
-        <Box><TrafficSignButton allow={!props.data.banRight} variant="sign" sign="→" onClick={() => props.onClick("banRight")}/></Box>
-        {props.data.leftHandTraffic && <Box><TrafficSignButton allow={!props.data.banUTurn} variant="sign" sign="↷" onClick={() => props.onClick("banUTurn")}/></Box>}
-        {!props.data.leftHandTraffic && <Box><TrafficSignButton allow={!props.data.banUTurn} variant="sign" sign="↶" onClick={() => props.onClick("banUTurn")}/></Box>}
+        <Box>
+          <TrafficSignButton
+            allow={!props.data.banLeft}
+            variant="sign"
+            sign="←"
+            onClick={() => props.onClick("banLeft")}
+            style={{opacity: opacity(!props.data.banLeft)}}
+          />
+        </Box>
+        <Box>
+          <TrafficSignButton
+            allow={!props.data.banStraight}
+            variant="sign"
+            sign="↑"
+            onClick={() => props.onClick("banStraight")}
+            style={{opacity: opacity(!props.data.banStraight)}}
+          />
+        </Box>
+        <Box>
+          <TrafficSignButton
+            allow={!props.data.banRight}
+            variant="sign"
+            sign="→"
+            onClick={() => props.onClick("banRight")}
+            style={{opacity: opacity(!props.data.banRight)}}
+          />
+        </Box>
+        {props.data.leftHandTraffic && (
+        <Box>
+          <TrafficSignButton
+            allow={!props.data.banUTurn}
+            variant="sign"
+            sign="↷"
+            onClick={() => props.onClick("banUTurn")}
+            style={{opacity: opacity(!props.data.banUTurn)}}
+          />
+        </Box>)}
+        {!props.data.leftHandTraffic && (
+        <Box>
+          <TrafficSignButton
+            allow={!props.data.banUTurn}
+            variant="sign"
+            sign="↶"
+            onClick={() => props.onClick("banUTurn")}
+            style={{opacity: opacity(!props.data.banUTurn)}}
+          />
+        </Box>)}
       </Container>
     </>
   );
